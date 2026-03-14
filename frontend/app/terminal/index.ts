16ad7307fad079d9206712400da0ead97fc9669d
David van Vliet <dv23@rice.edu>
2026-03-14 01:14:20 -0700

    changed the order of some help commands

--- frontend/app/terminal/index.ts
+++ frontend/app/terminal/index.ts
@@ -1,12 +1,12 @@
 import "./commands/help";
 import "./commands/clear";
 import "./commands/response";
+import "./commands/blog";
 import "./commands/socials";
 import "./commands/resume";
 import "./commands/globe";
 import "./commands/radarcorp";
-import "./commands/blog";
-import "./commands/films";
 import "./commands/books";
+import "./commands/films";
 
 export { getCommand, getAllCommands } from "./registry";
