251ba01887404295f949b57b2744c505bd0c9597
David van Vliet <dv23@rice.edu>
2026-05-02 22:47:46 -0400

    moon

--- frontend/app/blog/posts/moon.ts
+++ frontend/app/blog/posts/moon.ts
@@ -4,33 +4,27 @@ registerPost({
   slug: "moon",
   title: "Moon",
   date: "2026-02-28",
-  content: `Recently, I have placed myself in the fortunate position of not needing to remind myself of the overview effect. I am constantly living it.
-
-We are on a rock, orbiting a massive, gaseous balancing act of gravity and explosion.
-
-We are neighbored by countless similar systems in every direction, collectively swirling around a core of matter that is yet incomprehensible to us.
-
-There is so much to do.
-
-Step 1. The Moon.
-
-If someone wanted to convince me of the existence of god, their best shot would be to continuously remind me of the wonders of our moon, and about how lucky we can consider ourselves to be for having it.
+  content: `If someone wanted to convince me of the existence of god, their best shot would be to continuously remind me of the wonders of our moon. About how lucky we can consider ourselves to be for having it.
 
 Why?
 
 Because the moon is our gate to the stars.
 
 1. The moon is close, and can be reached at any time. There is no need to wait for orbits to align nor to make month-long commutes.
-2. The moon has no atmosphere and has low gravity. This means that launching and landing on the moon becomes exponentially cheaper and faster and better.
-3. The moon is rich in resources. Such as: Aluminium (one piece of British spelling I won't give up), Iron, Gold and Uranium. As well as ice in its deep craters and poles.
+2. The moon has no atmosphere and has low gravity. This means that launching and landing on the moon becomes exponentially cheaper and faster.
+3. The moon is rich in resources. Such as: aluminium (one piece of British spelling I won't give up), iron, gold, and uranium. It also contains water ice in its deep craters and poles. (I do not include tritium here. But it is likely there, lying in wait for all you fusion fanatics.)
 
-These characteristics make it the perfect place to turn into a giant orbiting space factory, ready to launch us toward new frontiers. An industrialized pipeline to reach both type I and to expand into the rest of the solar system as needed.
+These characteristics make it the perfect place to turn into a giant orbiting space factory, ready to launch us toward new frontiers. An industrialized pipeline to reach Type I and expand into the rest of the solar system as needed.
 
-Why is this important? Well, you can say that as long as we consider it important to have greater standards of living, greater freedom, more abilities, and a deeper understanding of nature, it is important to expand spatially, accelerate technologically, and most of all, consume more energy.
+Why is this important? Well, I will say that as long as we consider it important to have higher standards of living, greater freedom, more abilities, and a deeper understanding of nature, it will be necessary to expand spatially, accelerate technologically, and most of all, consume more energy.
 
 And the moon is our gate and launchpad to this prosperity.
 
-The next time you feel a lack of purpose when you're engineering a better adtech or an automated UGC farm, pull up a deep field image, overvieweffectmaxx, and remind yourself of just how much needs to get done to make our wildest dreams come true.
+But there is so much to do.
 
-So, why am I personally building investment-tech rather than going to the Moon. Well, as much as I believe in the value of facilitating efficient capital flow, I admit I should consider the question of "why wait?" more carefully.`,
+The next time you feel a lack of purpose when engineering the 100th ‘agentic UGC farm’, pull up a deep field image, overvieweffectmaxx, and remind yourself of just how much needs to get done to make our wildest dreams come true.
+
+To even make a dent in this, a level of urgency is required. However, this industrialization is a purposeful spear towards a future of good. This makes the struggle towards it the most relaxing, guilt-free, and prideworthy endeavor.
+
+So, why am I personally building investment-tech rather than mining the Moon? Well, I don’t have an answer yet. As much as I believe in the value of facilitating efficient capital flow, I admit I should consider the question of "why wait?" more carefully.`,
 });
